5b0406403f4bc5877c11da8d93cc4554288f80a2
Vikramsagar Ampaiah (Photoninfotech) <vikramsagar.ampaiah@photoninfotech.net>
2024-12-30 15:26:17 +0100

    Listing the default NGO

---
+++ Catalyst4Change.Web/NGOApiClient.cs
@@ -0,0 +1,44 @@
+﻿using Catalyst4Change.ApiService.Data;
+
+namespace Catalyst4Change.Web.Services
+{
+    public class NGOApiClient
+    {
+        private readonly HttpClient _httpClient;
+
+        public NGOApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<NGO>> GetNGOsAsync()
+        {
+            return await _httpClient.GetFromJsonAsync<List<NGO>>("api/NGO");
+        }
+
+        public async Task<NGO> GetNGOAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<NGO>($"api/NGO/{id}");
+        }
+
+        public async Task<NGO> CreateNGOAsync(NGO ngo)
+        {
+            var response = await _httpClient.PostAsJsonAsync("api/NGO", ngo);
+            response.EnsureSuccessStatusCode();
+
+            return await response.Content.ReadFromJsonAsync<NGO>();
+        }
+
+        public async Task UpdateNGOAsync(int id, NGO ngo)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"api/NGO/{id}", ngo);
+            response.EnsureSuccessStatusCode();
+        }
+
+        public async Task DeleteNGOAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"api/NGO/{id}");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
--- Catalyst4Change.Web/Program.cs
+++ Catalyst4Change.Web/Program.cs
@@ -1,5 +1,6 @@
 using Catalyst4Change.Web;
 using Catalyst4Change.Web.Components;
+using Catalyst4Change.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,6 +19,13 @@ builder.Services.AddHttpClient<WeatherApiClient>(client =>
         // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
         client.BaseAddress = new("https+http://apiservice");
     });
+
+builder.Services.AddHttpClient<NGOApiClient>(client =>
+{
+    // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
+    // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
+    client.BaseAddress = new("https+http://apiservice");
+});
 
 var app = builder.Build();
 
